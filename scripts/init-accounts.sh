6b04d0073f455499e8327faca74822414da67c8c
NicoDFS <admin@majorleaguedfs.com>
2019-07-29 16:47:31 -0400

    name

--- scripts/init-accounts.sh
+++ scripts/init-accounts.sh
@@ -1,13 +1,13 @@
 while
-     qcli help &> /dev/null
+     fcli help &> /dev/null
      rc=$?; if [[ $rc == 0 ]]; then break; fi
 do :;  done
 
-balance=`qcli getbalance`
+balance=`fcli getbalance`
 if [ "${balance:0:1}" == "0" ]
 then
     set -x
-	qcli generate 600 > /dev/null
+	fcli generate 600 > /dev/null
 	set -
 fi
 
@@ -15,13 +15,13 @@ LOCKFILE=${FantasyGold_DATADIR}/import-test-wallet.lock
 
 if [ ! -e $LOCKFILE ]; then
   while
-       qcli getaddressesbyaccount "" &> /dev/null
+       fcli getaddressesbyaccount "" &> /dev/null
        rc=$?; if [[ $rc != 0 ]]; then continue; fi
 
        set -x
 
-       qcli importprivkey "cMbgxCJrTYUqgcmiC1berh5DFrtY1KeU4PXZ6NZxgenniF1mXCRk" # addr=qUbxboqjBRp96j3La8D1RYkyqx5uQbJPoW hdkeypath=m/88'/0'/1'
-       qcli importprivkey "cRcG1jizfBzHxfwu68aMjhy78CpnzD9gJYZ5ggDbzfYD3EQfGUDZ" # addr=qLn9vqbr2Gx3TsVR9QyTVB5mrMoh4x43Uf hdkeypath=m/88'/0'/2'
+       fcli importprivkey "cMbgxCJrTYUqgcmiC1berh5DFrtY1KeU4PXZ6NZxgenniF1mXCRk" # addr=qUbxboqjBRp96j3La8D1RYkyqx5uQbJPoW hdkeypath=m/88'/0'/1'
+       fcli importprivkey "cRcG1jizfBzHxfwu68aMjhy78CpnzD9gJYZ5ggDbzfYD3EQfGUDZ" # addr=qLn9vqbr2Gx3TsVR9QyTVB5mrMoh4x43Uf hdkeypath=m/88'/0'/2'
 
        solar prefund qUbxboqjBRp96j3La8D1RYkyqx5uQbJPoW 500
        solar prefund qLn9vqbr2Gx3TsVR9QyTVB5mrMoh4x43Uf 500
